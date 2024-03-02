ebc2f1907f6eedd13ad54a9c0db4ee095e1e9297
GabrielVCB <gabrielvcb7@gmail.com>
2024-03-02 16:24:28 -0300

    Done

--- Atividade 03 - Conceitos básicos de Java - Programação III/Questao04.java
+++ Atividade 03 - Conceitos básicos de Java - Programação III/Questao04.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Questao02 {
+public class Questao04 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n1 = sc.nextInt();
