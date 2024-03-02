83b3d06936c1169f363a4fa9360307e637f17d6e
GabrielVCB <gabrielvcb7@gmail.com>
2024-03-02 16:25:05 -0300

    Done

---
+++ Atividade 03 - Conceitos básicos de Java - Programação III/Questao06.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Questao06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextDouble();
+        double quadrado = n * n;
+        System.out.printf("Quadrado = %f", quadrado);
+        sc.close();
+    }
+}
