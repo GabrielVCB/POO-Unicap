5b3f9b59d8494bedd32e75dcc9396b6f65fe752d
gabrielvcb7 <gabriel.00000849430@unicap.br>
2024-05-21 10:11:53 -0300

    Done

--- Atividade 13 - Tratamento de exceções/Q01_14.java
+++ Atividade 13 - Tratamento de exceções/Q01_14.java
@@ -11,13 +11,9 @@ public class Q01_14 {
             System.out.print("Digite um número: ");
             int posicao = scanner.nextInt();
 
-            if (posicao < 0 || posicao >= nome.length()) {
-                System.out.println("Posição inválida");
-            } else {
-                char caractere = nome.charAt(posicao);
-                System.out.println("O caractere na posição " + posicao + " é: " + caractere);
-            }
-        } catch (Exception e) {
+            char caractere = nome.charAt(posicao);
+            System.out.println("O caractere na posição " + posicao + " é: " + caractere);
+        } catch (IndexOutOfBoundsException e) {
             System.out.println("Posição inválida");
         } finally {
             scanner.close();
